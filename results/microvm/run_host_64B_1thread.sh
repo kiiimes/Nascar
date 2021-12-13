479d79f29624e5f590cc50bc3d79279b60404004
kiiimes <dkdla58@gmail.com>
2021-12-13 15:04:20 +0900

    rasp scripts

---
+++ results/microvm/run_host_64B_1thread.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#netperf -H 10.0.0.10 -p 1 -l 60 -- -m 64 > tcp_nonxdp1.txt &
+netperf -H 10.0.0.10 -l 60 -p 1 -- -m 64 > netperf1.txt &
+vnstat -tr 55 -i eth0 >> vnstat_eth0_3.txt &
+sshpass -p 1 ssh -t -t oslab@[localhost IP] "cd /home/oslab/eskim/results && echo '1' | sudo -S sh run_host.sh"
+scp *.txt oslab@[localhost IP]:/home/oslab/eskim/results
